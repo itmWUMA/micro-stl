3b997f921545cc90b1ef321dba07652f9f9e248a
itmWuma <2913850205@qq.com>
2022-07-10 18:13:58 +0800

    实现allocator

--- MicroSTL/allocator.hpp
+++ MicroSTL/allocator.hpp
@@ -1,10 +1,26 @@
 // 空间分配器: 用以调度内存
 #define _CRT_SECURE_NO_WARNINGS
 
+#include <assert.h>
 namespace mstl_itm
 {
+	template <class _AllocT>
 	class Allocator
 	{
+	public:
+		using AllocType	= _AllocT;
+		using Pointer		= _AllocT*;
 
+	public:
+		static Pointer Allocate()
+		{
+			return new AllocType();
+		}
+
+		static void Deallocate(Pointer p)
+		{
+			assert(p != nullptr);
+			delete p;
+		}
 	};
 }
