961eb7bf14c6dbe4066e533337f7d741a0f4cb2b
itmWuma <2913850205@qq.com>
2022-07-10 18:22:00 +0800

    实现allocator

--- MicroSTL/allocator.hpp
+++ MicroSTL/allocator.hpp
@@ -8,19 +8,35 @@ namespace mstl_itm
 	class Allocator
 	{
 	public:
-		using AllocType	= _AllocT;
-		using Pointer		= _AllocT*;
+		using AllocType	= _AllocT;	// 类型
+		using Pointer		= _AllocT*;	// 指针
+		using SizeType		= size_t;	// 大小类型
 
 	public:
+		// 分配一块内存空间
 		static Pointer Allocate()
 		{
 			return new AllocType();
 		}
 
+		// 分配连续内存空间
+		static Pointer AllocateRange(SizeType size)
+		{
+			return new AllocType[size];
+		}
+
+		// 释放一块内存空间
 		static void Deallocate(Pointer p)
 		{
 			assert(p != nullptr);
 			delete p;
+		}
+
+		// 释放连续内存空间
+		static void DeallocateRange(Pointer p)
+		{
+			assert(p != nullptr);
+			delete[] p;
 		}
 	};
 }
